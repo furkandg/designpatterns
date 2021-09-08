94a69da939958464dbee15a0de4025f06fb60f82
== <=>
2021-09-08 22:30:22 +0300

    singleton

---
+++ SingletonDesignPatterns/SingletonDesignPatterns/SingletonDesignPatterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using SingletonDesignPatterns.SingletonDesignPattern;
+using System;
+
+namespace SingletonDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbHelper ilk = DbHelper.CreateDbHelper("Oluşturuldu");
+            ilk.StateMessage = "Aşama 1";
+            Console.WriteLine(ilk.StateMessage);
+
+            DbHelper iki = DbHelper.CreateDbHelper("Bağlantı");
+
+
+            Console.WriteLine(iki.StateMessage);
+        }
+    }
+}
---
+++ SingletonDesignPatterns/SingletonDesignPatterns/SingletonDesignPatterns/SingletonDesignPattern/DbHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPatterns.SingletonDesignPattern
+{
+   public class DbHelper
+    {
+        public string ConnectionString { get; set; }
+        private DbHelper(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private static DbHelper _helper;
+
+        public static DbHelper CreateDbHelper(string connectionString)
+        {
+
+            if (_helper==null)
+            {
+                _helper = new DbHelper(connectionString);
+            }
+            return _helper;
+
+        }
+
+        public string StateMessage { get; set; }
+    }
+}
