3d4665efe38f0b7a933fd764990cfe9440484421
furkandg <80524882+furkandg@users.noreply.github.com>
2022-01-23 22:16:44 +0300

    change

--- StrategyDesignPattern - Kopya/StrategyDesignPattern/StrategyDesignPattern/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using StrategyDesignPattern.StrategyPattern;
-
-
-IStrategy selectedStrategy = ConverterFactory.CreateConverterClass("mp3");
-selectedStrategy.Convert();
-Console.ReadKey();
